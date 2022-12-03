64e677fa906c7633cee9603c1f0a37a1f8e1acd7
Nicholas Dylan <dylanlienardi@gmail.com>
2022-12-03 11:41:21 +0700

    progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,47 @@
 
 @section('content')
 
-<h1 class="text-3xl font-bold underline">
-    This is index
-</h1>
-  
+
+{{-- this is masthead hero section--}}
+<div class="hero min-h-screen" style="background-image: url(https://placeimg.com/1000/800/arch);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+        <div class="hero min-h-screen">
+
+            <div class="hero-content flex-col lg:flex-row text-black">
+                <img src="https://placeimg.com/1000/800/arch" class="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+{{-- this is history of company intro dll --}}
+
+<div id="2" class="min-h-screen bg-red-200" >
+
+</div>
+
+{{-- this is contact info --}}  
+
+<div id="3" class="min-h-screen bg-blue-200">
+
+</div>
+
+{{-- this is the scroll button--}}
+
+<div class="dropdown dropdown-hover dropdown-top fixed bottom-6 left-6 hidden lg:block opacity-60 hover:opacity-100 scroll-smooth">
+    <label tabindex="0" class="btn m-1 h-16 w-16 rounded-full">arrow</label>
+    <ul tabindex="0" class="dropdown-content menu p-4 shadow bg-white rounded-box">
+      <li><a href="#1">o</a></li>
+      <li><a href="#2">o</a></li>
+      <li><a href="#3">o</a></li>
+    </ul>
+</div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', [
+        'title' => 'Seger Waras'
+    ]);
 });
 
 Route::get('/dashboard', function () {
