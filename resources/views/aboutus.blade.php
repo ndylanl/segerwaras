08eebac04934d41ff351387c8ed5597166a76946
Nicholas Dylan <dylanlienardi@gmail.com>
2022-12-08 17:10:18 +0700

    create simple routing and views

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.template')
+
+@section('title', $title)
+
+@section('content')
+
+
+
+<div class="bg-neutral-50">
+    <div class="inline-flex justify-center items-center w-full lg:mt-20 mt-16">
+        <hr class="my-8 w-[50rem] h-px bg-green-800 border-0">
+        <span class="absolute left-1/2 px-3 text-5xl font-black bg-neutral-50 text-gray-900 -translate-x-1/2">VISI & MISI</span>
+    </div>
+</div>
+
+
+@endsection
