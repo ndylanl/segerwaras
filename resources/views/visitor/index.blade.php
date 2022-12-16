a9a2657a85290fb0fee1022dcae6e91ce9abf9da
Nicholas Dylan <dylanlienardi@gmail.com>
2022-12-16 19:25:10 +0700

    seeded bitch

--- resources/views/visitor/index.blade.php
+++ resources/views/visitor/index.blade.php
@@ -30,7 +30,7 @@
                         </div>
                         <div class="w-full lg:w-1/2 relative p-4">
                             <h4 class="text-3xl font-bold">{{ $product->name }}</h4>
-                            <p class="mt-3">{{ $product->description }}</p>
+                            <p class="mt-16">{{ $product->description }}</p>
                             <button class="lg:absolute lg:bottom-1 mt-5 text-white bg-green-400 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">See More</button>
                         </div>
                     </div>
