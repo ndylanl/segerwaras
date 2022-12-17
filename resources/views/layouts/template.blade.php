2ab65b2651f5ffa56b41ae8aeb18bde154d0adf7
Nicholas Dylan <dylanlienardi@gmail.com>
2022-12-17 14:13:15 +0700

    fuck it

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -76,6 +76,10 @@
                 <img src="/img/cartIcon.png" alt="" class="mx-2">
             </a>
 
+            <a href="/profile" class="hidden lg:block text-white mx-3 bg-green-400 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                Profile
+            </a>
+
             <a href="/logout" class="hidden lg:block text-white mx-3 bg-green-400 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                 Logout
             </a>
