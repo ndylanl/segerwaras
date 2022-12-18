d178ec9a0fce02afe872508f643d303156434de4
RDJinTropicThunder <nchristian02@student.ciputra.ac.id>
2022-12-18 14:04:21 +0700

    seed

--- database/seeders/DistributorSeeder.php
+++ database/seeders/DistributorSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Distributor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DistributorSeeder extends Seeder
 {
@@ -15,6 +16,16 @@ class DistributorSeeder extends Seeder
      */
     public function run()
     {
-        Distributor::Factory()->count(10)->create();
+        //Distributor::Factory()->count(10)->create();
+        DB::table('distributors')->insert([
+            'name' => "Surya Medical",
+            'description'=> "Melayani pengobatan secara general serta beauty products",
+            'user_id'=> 1
+        ]);
+        DB::table('distributors')->insert([
+            'name' => "Lunar Medical",
+            'description'=> "Tersedia produk medis herbal dari tradisional hingga modern",
+            'user_id'=> 1
+        ]);
     }
 }
